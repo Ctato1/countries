188d87b52b675d04683f05d9f34d09fec37be7b4
Ctato1 <dznelashvilitato@gmail.com>
2024-07-11 00:19:29 +0400

    fix bug7

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -8,21 +9,27 @@ import {Component, OnInit} from '@angular/core';
 export class HeaderComponent implements OnInit {
   isDark: boolean = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  }
+
   ngOnInit() {
-    const theme:string | null = localStorage.getItem('theme')
-    if(theme === 'dark'){
-      document.documentElement.setAttribute("data-theme",'dark');
-      this.isDark = true;
-    }else{
-      document.documentElement.setAttribute("data-theme",'light');
-      this.isDark = false;
+    if (isPlatformBrowser(this.platformId)) {
+      const theme: string | null = localStorage.getItem('theme')
+      if (theme === 'dark') {
+        document.documentElement.setAttribute("data-theme", 'dark');
+        this.isDark = true;
+      } else {
+        document.documentElement.setAttribute("data-theme", 'light');
+        this.isDark = false;
+      }
     }
+
   }
 
   onChangeMode() {
     this.isDark = !this.isDark;
     document.documentElement.setAttribute("data-theme", this.isDark ? 'dark' : 'light');
-    localStorage.setItem('theme', this.isDark ? 'dark': 'light')
+    localStorage.setItem('theme', this.isDark ? 'dark' : 'light')
   }
 
 }

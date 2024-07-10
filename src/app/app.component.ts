192c968a02d9335dc72c1665ceee1c725d8968c5
Ctato1 <dznelashvilitato@gmail.com>
2024-07-11 00:50:46 +0400

    fix bug10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {CountriesService} from "./shared/countries.service";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit{
 
+  constructor(private countriesService:CountriesService) {
+  }
   ngOnInit() {
+    this.countriesService.getData().subscribe(res=>{
+      return res;
+    })
   }
 }
